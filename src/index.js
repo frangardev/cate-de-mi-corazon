47f7fc55e1e938bec22bec138ea80c3c0d6be5d2
Francisco García <francoffee57@gmail.com>
2021-12-06 20:41:14 -0600

    página de cate de mi corazon terminada

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const API = 'https://platzi-avo.vercel.app'
+const appNode = document.querySelector("#container")
+appNode.className ="container"
 
-console.log('Happy hacking :)')
+//Esta funcion se encargara de internacionalizar los precios 
+const formatPrice = (price)=>{
+
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD"
+    }).format(price)
+
+    return newPrice
+}
+
+
+
+//Usando fetch
+window.fetch(`${API}/api/avo`)
+    .then((rta) => rta.json())
+    .then(rtaJson => {
+        console.log(rtaJson);
+        const rtaItems = []
+        
+        rtaJson.data.forEach(item => {
+            const image = document.createElement("img")
+            const title = document.createElement("h2")
+            const price = document.createElement("div")
+
+            image.src = API + item.image
+            title.textContent = item.name
+            price.textContent = formatPrice(item.price)
+            price.className = "price"
+
+            const container = document.createElement("div")
+            container.className = "item__container"
+            container.append(image, title, price)
+
+            rtaItems.push(container)
+            console.log(container);
+        })
+        appNode.append(...rtaItems)
+    })
+
